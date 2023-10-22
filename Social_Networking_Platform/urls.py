68d9c22a5cd088e719aaf042f6a33cb88841c3ca
himok <himok.pk@gmail.com>
2023-10-22 14:52:06 +0200

    Readme file

--- Social_Networking_Platform/urls.py
+++ Social_Networking_Platform/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for Social_Networking_Platform project.
+URL configuration for 2 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
---
+++ readme.md
@@ -0,0 +1,10 @@
+### Django First Run
+python -m venv venv
+.\venv\Scripts\activate
+pip install -r requirements.txt
+python manage.py runserver
+
+### React First Run
+cd frontend
+npm install
+npm run start
