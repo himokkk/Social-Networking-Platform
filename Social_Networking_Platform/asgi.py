c03e5c25dd3e8254f2ab5f56eb558571aaf3e5a1
himok <himok.pk@gmail.com>
2023-10-22 14:57:33 +0200

    Adding restframework

--- Social_Networking_Platform/asgi.py
+++ Social_Networking_Platform/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for 2 project.
+ASGI config for Social_Networking_Platform project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', '2.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Social_Networking_Platform.settings')
 
 application = get_asgi_application()
--- Social_Networking_Platform/wsgi.py
+++ Social_Networking_Platform/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for 2 project.
+WSGI config for Social_Networking_Platform project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', '2.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Social_Networking_Platform.settings')
 
 application = get_wsgi_application()
