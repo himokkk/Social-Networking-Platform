b21124514e953c67ad006b39e13be18054a8f5cd
Łukasz Łukaszewski <lukaszmlukaszewski@gmail.com>
2023-12-10 12:48:33 +0100

    added API schema

--- Social_Networking_Platform/settings.py
+++ Social_Networking_Platform/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework_simplejwt",
+    "drf_spectacular",
     "posts",
     "users",
 ]
@@ -47,7 +48,8 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-    )
+    ),
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
 MIDDLEWARE = [
@@ -138,3 +140,15 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 #     'ACCESS_TOKEN_LIFETIME': datetime.timedelta(minutes=5),
 #     'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=1),
 # }
+
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Social Networking Platform API',
+    'SERVE_PERMISSIONS': ['rest_framework.permissions.IsAdminUser'],
+    'SERVE_AUTHENTICATION': ['rest_framework.authentication.BasicAuthentication'],
+    'SWAGGER_UI_SETTINGS': {
+        'deepLinking': True,
+        'persistAuthorization': True,
+        'displayOperationId': True,
+    },
+}
--- Social_Networking_Platform/urls.py
+++ Social_Networking_Platform/urls.py
@@ -2,9 +2,23 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("posts/", include("posts.urls")),
     path("user/", include("users.urls")),
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path(
+        'schema/swagger-ui/',
+        SpectacularSwaggerView.as_view(url_name='schema'),
+        name='swagger-ui',
+    ),
+    path(
+        'schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
