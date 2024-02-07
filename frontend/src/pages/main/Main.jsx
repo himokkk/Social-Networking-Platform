459d422bfff7db454cb6578940ccb139b48d0873
Nikolai Lobchuk <nikolai.lobchuk@gmail.com>
2024-02-07 15:11:35 +0100

    Added logout,finder,render profile

--- frontend/src/pages/main/Main.jsx
+++ frontend/src/pages/main/Main.jsx
@@ -1,115 +1,130 @@
-import React, { useState ,useEffect } from 'react';
-import Post from './post/Post';
-import Notification from './notification/Notification';
-import MyProfile from './myprofile/MyProfile';
-import './Main.css';
-import { FaUser, FaEnvelope, FaHome, FaBell, FaPlus, FaArrowLeft} from 'react-icons/fa';
-import { useNavigate } from 'react-router-dom';
-import { getData } from "../../functions/getData";
-import { LOGIN_URL } from "../../urls";
-import { refreshAccess } from "../../functions/refreshAccess";
+import React, { useEffect, useState } from 'react';
+import { FaUsers, FaArrowLeft, FaBell, FaHome, FaPlus, FaUser, FaUserTimes } from 'react-icons/fa';
+import Modal from 'react-modal';
 import PerfectScrollbar from 'react-perfect-scrollbar';
-import 'react-perfect-scrollbar/dist/css/styles.css'
+import 'react-perfect-scrollbar/dist/css/styles.css';
+import { useNavigate } from 'react-router-dom';
 import { apiCall } from '../../functions/apiCall';
 import { filterResponse } from "../../functions/filterResponse";
-import Modal from 'react-modal';
+import { getData } from "../../functions/getData";
+import { refreshAccess } from "../../functions/refreshAccess";
+import { LOGIN_URL } from "../../urls";
+import './Main.css';
+import MyProfile from './myprofile/MyProfile';
+import Notification from './notification/Notification';
+import Post from './post/Post';
+import UserFinder from './userfinder/UserFinder';
+import Profile from './profile/Profile';
+import { deleteAllCookies } from "../../functions/deleteAllCookies";
 
 
 function Main() {
-  const navigate = useNavigate();
-  const [selectedCategory, setSelectedCategory] = useState('posts');
-  const [postResults, setPostResults] = useState([]);
-  const [notificationResults, setNotificationResults] = useState([]);
-  const [myProfileResults, setMyProfileResults] = useState([]);
-  const [showCreatePostModal, setShowCreatePostModal] = useState(false);
-  
-  const handleCategoryClick = (category) => {
+    const navigate = useNavigate();
+    const [selectedCategory, setSelectedCategory] = useState('posts');
+    const [postResults, setPostResults] = useState([]);
+    const [notificationResults, setNotificationResults] = useState([]);
+    const [myProfileResults, setMyProfileResults] = useState([]);
+    const [profileResults, setProfileResults] = useState([]);
+    const [userFinderResults, setUserFinderResults] = useState(null);
+    const [showCreatePostModal, setShowCreatePostModal] = useState(false);
+
+
+    const handleUserFinderResults = (results) => {
+        setUserFinderResults(results);
+        setSelectedCategory("profile");
+    };
+
+    const handleCategoryClick = (category) => {
     setSelectedCategory(category);
-  };
-  const handleDeletePost  =  async (postId) =>
-  {
+    };
+
+    const handleDeletePost  =  async (postId) =>
+    {
     {checkIfLoggedIn}
     await apiCall(`http://localhost:8000/posts/${postId}/delete`, "DELETE");
     setPostResults(prevPostResults => prevPostResults.filter(post => post.id !== postId));
-  }
+    }
 
-  useEffect(() => {
-    if (selectedCategory === 'posts') {
-      getData("http://localhost:8000/posts/explore")
-        .then((data) => {
-          renderPosts(data.results);
-          setPostResults(data.results); 
-        })
-        .catch((error) => {
-          console.error("Error getting data:", error);
-        });
-      }
-    //else if (selectedCategory === 'notifications') {
-    //  getData("http://localhost:8000/notifications/explore")
-    //    .then((data) => {
-    //      renderNotifications(data.results);
-    //      setNotificationResults(data.results);
-    //    })
-    //    .catch((error) => {
-    //      console.error("Error getting notifications:", error);
-    //    });
-    //}
-    else if (selectedCategory === 'notifications') {
-        getFakeNotifications()
+    useEffect(() => {
+        if (selectedCategory === 'posts') {
+            getData("http://localhost:8000/posts/explore")
+                .then((data) => {
+                    renderPosts(data.results);
+                    setPostResults(data.results);
+                })
+                .catch((error) => {
+                    console.error("Error getting data:", error);
+                });
+        }
+        else if (selectedCategory === 'notifications') {
+            const fetchData = async () => {
+                try {
+                    const response = await apiCall(`http://localhost:8000/user/notification/list`, "GET");
+                    const responseResults = await response.json();
+                    renderNotifications(responseResults);
+                    console.log(responseResults);
+                    setNotificationResults(responseResults);
+                } catch (error) {
+                    console.error("Error getting notifications:", error);
+                }
+            }
+            fetchData();
+        }
+        else if (selectedCategory === 'my_profile') {
+            const fetchData = async () => {
+                try {
+
+                    const response = await apiCall(`http://localhost:8000/user/current/`, "GET");
+                    const responseResults = await filterResponse(response, ["id", "description", "birth", "image_url", "username", "friends"]);
+                    console.log(responseResults);
+                    renderMyProfile(responseResults);
+                    setMyProfileResults(responseResults);
+                } catch (error) {
+                    console.error("Error:", error);
+                }
+            };
+
+            fetchData();
+        }
+        else if (selectedCategory === 'user_finder') {
+            getData("http://localhost:8000/user/search/")
+                .then((data) => {
+                    renderUserFinder(data);
+                })
+                .catch((error) => {
+                    console.error("Error getting data:", error);
+                });
+        }
+        else if (selectedCategory === 'profile') {
+        getData(`http://localhost:8000/user/${userFinderResults}`)
             .then((data) => {
-                console.log('Fake notifications:', data);
-                renderNotifications(data);
-                setNotificationResults(data);
+                renderProfile(data);
+                setProfileResults(data);
             })
             .catch((error) => {
-                console.error('Error getting fake notifications:', error);
+                console.error("Error getting data:", error);
             });
-      }
-    else if (selectedCategory === 'myprofile') {
-        const fetchData = async () => {
-            try {
-
-                const response = await apiCall(`http://localhost:8000/user/current/`, "GET");
-                const responseResults = await filterResponse(response, ["id", "description", "birth", "image_url", "username"]);
-                renderMyProfile(responseResults);
-                console.log(responseResults);
-                setMyProfileResults(responseResults);
-            } catch (error) {
-                console.error("Error:", error);
-            }
-        };
-
-        fetchData();
-    }
-  }, [selectedCategory]);
-
-    // Placeholder function to get fake notifications for testing
-    const getFakeNotifications = async () => {
-        try {
-            const fakeNotifications = [
-                { id: 1, type: 'like', content: 'Someone liked your post', link: '/posts/123' },
-                { id: 2, type: 'comment', content: 'You have a new comment', link: '/posts/456#comment-789' },
-            ];
-
-            return fakeNotifications;
-        } catch (error) {
-            console.error('Error during fetching fake notifications:', error);
-            throw error;
         }
-    };
+    else if (selectedCategory === 'logout') {
+        deleteAllCookies();
+        navigate(LOGIN_URL);
+    }
+    }, [selectedCategory]);
 
-  const renderPosts = (postResults) => {
+
+
+    const renderPosts = (postResults) => {
     return (
-      <>
+        <>
         {postResults.map(post => (
-          <div key={post.id}>
+            <div key={post.id}>
             <Post data={post} onDelete={handleDeletePost}/>
         
-          </div>
+            </div>
         ))}
-      </>
+        </>
     );
-  };
+    };
 
     const renderNotifications = (notificationResults) => {
         return (
@@ -122,90 +137,107 @@ function Main() {
             </>
         );
     };
-    const renderMyProfile = (myProfileResults) => {
+
+    const renderMyProfile = (myProfileResults ) => {
         return (
             <MyProfile data={myProfileResults} />
         );
     };
 
-  const checkIfLoggedIn = async () => {
-    let loggedIn = await refreshAccess()
-    if (!loggedIn) {
-        navigate(LOGIN_URL)
+    const renderProfile = (profileResults) => {
+        return (
+            <Profile data={profileResults} />
+        );
+    };
+
+    const renderUserFinder = () => {
+        return (
+            <UserFinder onUserFinderResults={handleUserFinderResults} />
+        );
+    };
+
+    const checkIfLoggedIn = async () => {
+        let loggedIn = await refreshAccess()
+        if (!loggedIn) {
+            navigate(LOGIN_URL)
+        }
     }
+
+    useEffect(() => {
+        checkIfLoggedIn()
+      }, []);
+
+    return (
+        <>
+            <div className={'upBar'}>
+            </div>
+            <div className={'leftBar'}>
+                <div onClick={() => handleCategoryClick('my_profile')}
+                    className='selectionDiv'>
+                    <p style={{ fontSize: '24px' }}>
+                        <FaUser size={32} /> My Profile
+                    </p>
+                </div>
+                <div onClick={() => handleCategoryClick('posts')}
+                    className='selectionDiv'>
+                    <p style={{ fontSize: '24px' }}>
+                        <FaHome size={32} /> Posts
+                    </p>
+                </div>
+                <div onClick={() => handleCategoryClick('notifications')}
+                    className='selectionDiv'>
+                    <p style={{ fontSize: '24px' }}>
+                        <FaBell size={32} /> Notifications
+                    </p>
+                </div>
+                <div onClick={() => handleCategoryClick('user_finder')}
+                    className='selectionDiv'>
+                    <p style={{ fontSize: '24px' }}>
+                        <FaUsers size={32} /> User Finder
+                    </p>
+                </div>
+                <div onClick={() => handleCategoryClick('logout')}
+                    className='selectionDiv'>
+                    <p style={{ fontSize: '24px' }}>
+                        <FaUserTimes size={32} /> Logout
+                    </p>
+                </div>
+            </div>
+      
+            <div className="content">
+                {selectedCategory === 'user_finder' && renderUserFinder()}
+                <PerfectScrollbar>
+                    {selectedCategory === 'my_profile' && (renderMyProfile(myProfileResults))}
+                    {selectedCategory === 'profile' && (renderProfile(profileResults))}
+                    {selectedCategory === 'notifications' && (renderNotifications([]) ? renderNotifications(notificationResults) : <p>No notifications available.</p>)}
+                    {selectedCategory === 'posts' && (renderPosts([]) ? renderPosts(postResults) : <p>No posts available.</p>)}
+                </PerfectScrollbar>
+            </div>
+            <FaPlus onClick={() => setShowCreatePostModal(prevState => !prevState)} />
+            {showCreatePostModal &&
+                <Modal
+                    isOpen={showCreatePostModal}
+                    onRequestClose={() => setShowCreatePostModal(false)}
+                    style={{
+                        content: {
+                            width: '500px',
+                            margin: 'auto',
+                            backgroundColor: '#f0f0f0',
+                            borderRadius: '10px',
+                            overflow: 'hidden',
+                        },
+                    }}>
+                    <div className="modal-header">
+                        <div onClick={() => setShowCreatePostModal(false)}>
+                            <FaArrowLeft />
+                        </div>
+                        <h3>Create Post</h3>
+                        <hr style={{ margin: '10px 0', border: '0.5px solid #ccc' }} />
+                    </div>
+                    <input type="file" accept="image/*" title="Choose file" />
+                </Modal>} 
+        </>
+    );
 }
 
-useEffect(() => {
-    checkIfLoggedIn()
-  }, []);
-
-  return (
-    <>
-      <div className={'upBar'}>
-        <input type="text" placeholder="Wyszukaj..." />
-      </div>
-      <div className={'leftBar'}>
-        <div onClick={() => handleCategoryClick('myprofile')}
-        className='selectionDiv'>
-          <p style={{ fontSize: '24px' }}>
-            <FaUser size={32} /> My Profile
-          </p>
-        </div>
-        <div onClick={() => handleCategoryClick('messages')}
-        className='selectionDiv'>
-          <p style={{ fontSize: '24px' }}>
-            <FaEnvelope size={32} /> Messages
-          </p>
-        </div>
-        <div onClick={() => handleCategoryClick('posts')}
-        className='selectionDiv'>
-          <p style={{ fontSize: '24px' }}>
-            <FaHome size={32} /> Posts
-          </p>
-        </div>
-        <div onClick={() => handleCategoryClick('notifications')}
-        className='selectionDiv'>
-          <p style={{ fontSize: '24px' }}>
-            <FaBell size={32} /> Notifications
-          </p>
-        </div>
-      </div>
-      
-    <div className="content">
-      <PerfectScrollbar>
-        {selectedCategory === 'myprofile' && (renderMyProfile(myProfileResults))}
-        {selectedCategory === 'messages' && <p>Wiadomości</p>}
-        {selectedCategory === 'notifications' && (renderNotifications([]) ? renderNotifications(notificationResults) : <p>no notifications available.</p>)}
-        {selectedCategory === 'posts' && (renderPosts([]) ? renderPosts(postResults) : <p>No posts available.</p>)}
-      </PerfectScrollbar>
-    </div>
-    <FaPlus onClick={() => setShowCreatePostModal(prevState => !prevState)} />
-    {showCreatePostModal && 
-    <Modal 
-    isOpen={showCreatePostModal}
-    onRequestClose={() => setShowCreatePostModal(false)}
-    style={{
-      content: {
-        width: '500px',
-        margin: 'auto',
-        backgroundColor: '#f0f0f0',
-        borderRadius: '10px',
-        overflow: 'hidden',
-      },
-    }}>
-       <div className="modal-header">
-       <div onClick={() => setShowCreatePostModal(false)}>
-         <FaArrowLeft />
-       </div>
-       <h3>Create Post</h3>
-       <hr style={{ margin: '10px 0', border: '0.5px solid #ccc' }} />
-     </div>
-     <input type="file" accept="image/*" title="Choose file" />
- 
-      </Modal>}
-      
-    </>
-  );
-}
-
-export default Main;
+    export default Main;
