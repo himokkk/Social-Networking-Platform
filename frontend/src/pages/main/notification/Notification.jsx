743630ea653f54670aca55eb70fbc482f56f1f1d
Nikolai Lobchuk <nikolai.lobchuk@gmail.com>
2024-02-08 04:23:45 +0100

    Updated notification

--- frontend/src/pages/main/notification/Notification.jsx
+++ frontend/src/pages/main/notification/Notification.jsx
@@ -1,40 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaUserPlus, FaThumbsUp, FaComment } from 'react-icons/fa';
 import './Notification.css';
 import { apiCall } from "../../../functions/apiCall";
 
-const Notification = ({ data }) => {
-    const handleNotificationClick = () => {
-        switch (data.notification_type) {
-            case 'friend_request':
-                console.log(data.from_user.id);
-                break;
-            case 'post_like':
-                console.log(data.post);
-                break;
-            case 'post_comment':
-                console.log(data.post);
-                break;
-            default:
-                break;
-        }
+const Notification = ({ data, onNotificationResultsUser }) => {
+    const [notificationData, setNotificationData] = useState(data);
+
+    const handleUserClick = (userId) => {
+        onNotificationResultsUser(userId);
     };
 
     const getNotificationText = () => {
-        switch (data.notification_type) {
+        switch (notificationData.notification_type) {
             case 'friend_request':
-                return `User ${data.from_user.username} sent you a friend request.`;
+                return (
+                    <>
+                        {"User"} 
+                        <span className="notification-user-name" onClick={() => handleUserClick(notificationData.from_user.id)}> {notificationData.from_user.username}</span>
+                        {" sent you a friend request."}
+                    </>
+                );
             case 'post_like':
-                return `User ${data.from_user.username} liked your post.`;
+                return `User ${notificationData.from_user.username} liked your post.`;
             case 'post_comment':
-                return `User ${data.from_user.username} commented on your post.`;
+                return `User ${notificationData.from_user.username} commented on your post.`;
             default:
                 return '';
         }
     };
 
     const getNotificationIcon = () => {
-        switch (data.notification_type) {
+        switch (notificationData.notification_type) {
             case 'friend_request':
                 return <FaUserPlus />;
             case 'post_like':
@@ -48,7 +44,10 @@ const Notification = ({ data }) => {
 
     const handleAcceptFriendRequest = async () => {
         try {
-            await apiCall(`http://localhost:8000/user/notification/${data.from_user.id}/accept`, "PATCH");
+            await apiCall(`http://localhost:8000/user/notification/${notificationData.id}/accept`, "PATCH");
+            const response = await apiCall(`http://localhost:8000/user/notification/list`, "GET");
+            const responseResults = await response.json();
+            setNotificationData(responseResults);
         }
         catch (error) {
             console.error('Error :', error);
@@ -56,17 +55,19 @@ const Notification = ({ data }) => {
     };
 
     return (
-        <div className="notification-container" onClick={handleNotificationClick}>
-            <div className="notification-icon">{getNotificationIcon()}</div>
-            <div className="notification-content">
-                <p className="notification-text">{getNotificationText()}</p>
-                {data.notification_type === 'friend_request' && (
-                    <button className="accept-friend-request-button" onClick={handleAcceptFriendRequest}>
-                        Accept
-                    </button>
-                )}
+        notificationData.from_user.id !== notificationData.to_user.id && (
+            <div className="notification-container" >
+                <div className="notification-icon">{getNotificationIcon()}</div>
+                <div className="notification-content">
+                    <p className="notification-text">{getNotificationText()}</p>
+                    {notificationData.notification_type === 'friend_request' && (
+                        <button className="accept-friend-request-button" onClick={handleAcceptFriendRequest}>
+                            Accept
+                        </button>
+                    )}
+                </div>
             </div>
-        </div>
+        )
     );
 };
 
