b905ede5f0e09d89774757e913a1d255df0c29e3
mldxo <miloszmaculewicz@gmail.com>
2024-03-31 20:04:42 +0200

    Remove old run method

--- backend/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-source /home/mlsh/social-networking-platform/backend/venv/bin/activate
-python /home/mlsh/social-networking-platform/backend/manage.py runserver 0.0.0.0:8000
--- frontend/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /home/mlsh/social-networking-platform/frontend
-serve -s build -n
